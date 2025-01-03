80a1d71bb190173081e5535d289e7fdef3fb2aac
CasteSoftworks <giacomo.castellana01@gmail.com>
2025-01-03 15:07:40 +0100

    aggiornamneto Politica.java per segnalare meglio l'eventuale lancio di IllegalArgumentException

--- progetto-cc2d71f-2025-01-01-16-07-32/src/main/java/BorsaNova/PoliticaPrezzo/Politica.java
+++ progetto-cc2d71f-2025-01-01-16-07-32/src/main/java/BorsaNova/PoliticaPrezzo/Politica.java
@@ -8,7 +8,9 @@ public interface Politica {
      * @param acquisto true se si tratta di un acquisto, false se si tratta di una vendita
      * 
      * @return il prezzo calcolato
+     * 
+     * @throws IllegalArgumentException se il valore che deve modificare prezzo è minore di 0
      */
-    public abstract int calcolaPrezzo(int prezzo, boolean acquisto);
+    public abstract int calcolaPrezzo(int prezzo, boolean acquisto) throws IllegalArgumentException;
     
 } 
