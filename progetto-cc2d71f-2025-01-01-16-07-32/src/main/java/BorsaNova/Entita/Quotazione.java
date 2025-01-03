6a20246c4e2b895be2415d325bdc3db4ae4a6824
CasteSoftworks <giacomo.castellana01@gmail.com>
2025-01-03 14:42:42 +0100

    aggiunta AF e RI per Quotazione.java, aggioranto metodo Quotazione

--- progetto-cc2d71f-2025-01-01-16-07-32/src/main/java/BorsaNova/Entita/Quotazione.java
+++ progetto-cc2d71f-2025-01-01-16-07-32/src/main/java/BorsaNova/Entita/Quotazione.java
@@ -4,6 +4,20 @@ package BorsaNova.Entita;
  * Classe per rappresentare una <p>quotazione</p>
  */
 public class Quotazione {
+    /**
+     * AF:
+     * AF(azienda, borsa, prezzoCorrente) = Una quotazione rappresentata da:
+     * - azienda: è l'azienda della quotazione
+     * - borsa: è la borsa della quotazione
+     * - prezzoCorrente: è il prezzo corrente della quotazione
+     *
+     * RI:
+     * RI(azienda, borsa, prezzoCorrente) = L'oggetto quotazione rispetta le seguenti condizioni:
+     * - azienda non è null e non è una stringa vuota o composta solo da spazi bianchi
+     * - borsa non è null e non è una stringa vuota o composta solo da spazi bianchi
+     * - prezzoCorrente è maggiore o uguale a 0
+     */
+
     /** L'azienda della quotazione */
     private final Azienda azienda;
     /** La borsa della quotazione */
@@ -19,10 +33,14 @@ public class Quotazione {
      * @param prezzo il prezzo corrente della quotazione
      * 
      * @throws NullPointerException se l'azienda o la borsa sono nulle
+     * @throws IllegalArgumentException se l'azienda o la borsa sono stringhe vuote o composte solo da spazi bianchi e se il prezzo è minore di 0 o nullo
      */
     public Quotazione(Azienda azienda, Borsa borsa, Integer prezzo){
         if(azienda==null || borsa==null){
             throw new NullPointerException("L'azienda e/o la borsa della quotazione non possono essere nulli");
+        }
+        if(azienda.getNome().isBlank() || borsa.getNome().isBlank()){
+            throw new IllegalArgumentException("L'azienda e/o la borsa della quotazione non possono essere stringhe vuote o composte solo da spazi bianchi");
         }
         if(prezzo<0||prezzo==null){
             throw new IllegalArgumentException("Il prezzo della quotazione non può essere negativo o nullo");
