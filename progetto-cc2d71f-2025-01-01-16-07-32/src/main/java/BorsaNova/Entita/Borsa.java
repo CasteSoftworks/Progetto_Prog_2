06edfbe0b76f8f0243eaf7849a2547836ae6bcf6
CasteSoftworks <giacomo.castellana01@gmail.com>
2025-01-04 12:44:13 +0100

    rimozione mappa di mappe azienda, (operatore, intero) per inutilità

--- progetto-cc2d71f-2025-01-01-16-07-32/src/main/java/BorsaNova/Entita/Borsa.java
+++ progetto-cc2d71f-2025-01-01-16-07-32/src/main/java/BorsaNova/Entita/Borsa.java
@@ -30,11 +30,7 @@ public class Borsa implements Comparable<Borsa>{
     private static ArrayList<Borsa> borse = new ArrayList<>();
     /** Mappa delle azioni totali(key= azienda, value= quantità di azioni) */
     private Map<Azienda, Integer> azioni = new HashMap<>();
-    /** Mappa delle azioni di ogni azienda allocate agli operatori (key= azienda, value= mappa di (key=operatori, value=quantità di azioni))
-     *  - la differenza tra azioni e azioniAziendaDiOperatori è la qunatità di azioni libere e acquistabili
-    */
-    private Map<Azienda, Map<Operatore, Integer>> azioniAziendaDiOperatori = new HashMap<>();
-    
+       
 
     /**
      * Metodo per costruire una borsa
